59b92d40a50f6d1414062c374b4ea2f7f37a4f1a
rp-guruh <github.guruh@gmail.com>
2025-07-03 17:28:01 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
+slint::include_modules!();
+
+fn print_hell(text: &str) -> &str {
+    text
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+    println!("{}", print_hell("Hello world!"));
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()?;
+
+    Ok(())
+}
