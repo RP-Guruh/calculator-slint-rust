ba404143cc2f63c28ac5f12ff56410eee0850543
rp-guruh <github.guruh@gmail.com>
2025-07-04 17:36:37 +0700

    fix interactive user

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,46 @@ fn print_hell(text: &str) -> &str {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
+    let ui_weak = ui.as_weak();
     println!("{}", print_hell("Hello world!"));
+
+    // append angka yang dipilih agar tampil ke layar 
+    ui.on_append({
+        let ui = ui_weak.clone();
+        move |val| {
+            if let Some(ui) = ui.upgrade() {
+                let mut expr = ui.get_expression();
+                expr.push_str(&val);
+                ui.set_expression(expr);
+            }
+        }
+    });
+
+    // trigger tombol backsapace
+    ui.on_backspace({
+        let ui = ui_weak.clone();
+        move || {
+            if let Some(ui) = ui.upgrade() {
+                let mut expr = ui.get_expression().to_string();
+                expr.pop();
+                ui.set_expression(expr.into());
+            }
+        }
+    });
+
+    ui.on_calculate({
+        let ui = ui_weak.clone();
+        move || {
+            if let Some(ui) = ui.upgrade() {
+                let expr = ui.get_expression();
+                match evalexpr::eval_int(&expr) {
+                    Ok(value) => ui.set_result(slint::SharedString::from(format!("{}", value))),
+                    Err(error) => ui.set_result(slint::SharedString::from(format!("Error: {}", error))),
+                }
+            }
+        }
+    });
+
     ui.on_request_increase_value({
         let ui_handle = ui.as_weak();
         move || {
